0d347923abf090acb96a40c40b23b1de9d4a2195
kevin-cassia <kevinyang@Kevins-MacBook-Pro.local>
2021-08-25 13:10:04 -0700

    Moved the file writing code to the module filewrite.py.

--- root/apps/container_ti_sensortag_example.py
+++ root/apps/container_ti_sensortag_example.py
@@ -1,21 +1,7 @@
 from cassiadevtools.cassia_api import CassiaApi
+from cassiadevtools import filewrite
 from collections import defaultdict
 import asyncio
-import shutil
-import os
-import warnings
-import sys
-
-# Data file size limits are in bytes.
-SIZE_100KB = 100000
-SIZE_500KB = 500000
-SIZE_1MB = 1000000
-SIZE_10MB = 10000000
-SIZE_100MB = 100000000
-SIZE_500MB = 500000000
-
-DATA_FILE_SIZE_LIMIT = SIZE_100KB
-MAX_USED_STORAGE_RATIO = 0.7
 
 API_DOMAIN_OR_IP_ADDRESS = '192.168.4.27'
 
@@ -36,61 +22,6 @@ async def connect_devices(api, paired_devices):
             await api.connect(paired_dev[0])  # Use the device MAC address to connect.
             paired_devices[paired_dev[0]] = 'connected'
 
-
-async def write_results_to_file(data, data_file_num_state):
-    total, used, free = shutil.disk_usage("/")
-    data_dir_path = '../data'
-    data_file_count = 3
-    cur_data_file_count = 0
-
-    try:
-        os.mkdir(data_dir_path)
-    except FileExistsError as e:
-        pass
-    except Exception as e:
-        print(e)
-        exit()
-
-    while (os.path.isfile(data_dir_path + 
-           '/data{count}.txt'.format(count=data_file_num))):
-        data_file_num += 1
-
-    if data_file_num == 1:
-        file_str = (
-            '{path}/data{count}.txt'.format(path=data_dir_path, 
-                                            count=data_file_num))        
-        with open(file_str,'x'):
-                print('Created data file: {}'.format(file_str))
-    else:
-        data_file_num -= 1
-
-    while cur_data_file_count < data_file_count:
-        if used < total * MAX_USED_STORAGE_RATIO:
-            if (sys.getsizeof(data) + os.path.getsize(
-                '{path}/data{count}.txt'.format(
-                    path=data_dir_path,
-                    count=data_file_num)) >= DATA_FILE_SIZE_LIMIT):
-
-                data_file_num += 1
-                file_str = (
-                    '{path}/data{count}.txt'.format(path=data_dir_path, 
-                                                    count=data_file_num))
-                with open(file_str,'x'):
-                    print('Created data file: {}'.format(file_str))
-
-                cur_data_file_count += 1
-
-            file_str = (
-                '{path}/data{count}.txt'.format(path=data_dir_path, 
-                                                count=data_file_num))
-
-            with open(file_str, 'a') as data_file:
-                data_file.write(data)
-        else:
-            warnings.warn(
-                ('Storage is {}"%" used! Data files cannot be created until '
-                 'space is freed.').format(MAX_USED_STORAGE_RATIO * 100),
-                ResourceWarning)
 
 async def main():
     api = CassiaApi('container', API_DOMAIN_OR_IP_ADDRESS)
