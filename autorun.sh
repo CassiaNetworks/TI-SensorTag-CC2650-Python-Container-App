1cb5487dd718287a340b5c24d9227f55fdd5d709
kevin-cassia <kevinyang@Kevins-MacBook-Pro.local>
2021-08-25 18:01:52 -0700

    Changed python3.9 to python3 so it can work in the v1.1.1 container by default.

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3.9 root/apps/container_ti_sensortag_example.py 2>&1 >/dev/null
+#python3.9 root/apps/container_ti_sensortag_example.py 2>&1 >/dev/null
+python3 root/apps/container_ti_sensortag_example.py 2>&1 >/dev/null
